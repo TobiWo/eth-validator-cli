332913f67ae43e51b23bf9fcccd8c4b71202b0e9
TobiWo <Emailkeeper@gmx.de>
2024-11-30 23:04:03 +0100

    feat(constants): Add constants

---
+++ src/constants/service.ts
@@ -0,0 +1,7 @@
+// CLI Validation constants
+export const VALID_URL_PREFIXES = ['http://', 'https://'];
+
+// Transaction fee calculation constants
+export const MIN_CONSOLIDATION_REQUEST_FEE = 1n;
+export const CONSOLIDATION_REQUEST_FEE_UPDATE_FRACTION = 17n;
+export const TRANSACTION_TIP = 10n;
