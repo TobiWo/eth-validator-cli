d0c159b5a4db359ff8f6953910e6081011252cf7
Tobias Wohland <Emailkeeper@gmx.de>
2024-12-06 17:27:59 +0100

    feat(config): Add mekong config

---
+++ src/network-config.ts
@@ -0,0 +1,9 @@
+import { NetworkConfig } from './model/ethereum';
+
+export const networkConfig: Record<string, NetworkConfig> = {
+  mekong: {
+    consolidationContractAddress: '0x01aBEa29659e5e97C95107F20bb753cD3e09bBBb',
+    withdrawalContractAddress: '',
+    chainId: 7078815900n
+  }
+};
