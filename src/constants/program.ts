afff50ee2a006d889cefc089d044dc4a03129c41
TobiWo <Emailkeeper@gmx.de>
2024-12-23 18:46:06 +0100

    feat(constants): Update service constants

--- src/constants/program.ts
+++ src/constants/program.ts
@@ -4,4 +4,7 @@ export const VALID_URL_PREFIXES = ['http://', 'https://'];
 // Transaction fee calculation constants
 export const MIN_CONSOLIDATION_REQUEST_FEE = 1n;
 export const CONSOLIDATION_REQUEST_FEE_UPDATE_FRACTION = 17n;
-export const TRANSACTION_TIP = 10n;
+
+// Request constants
+export const BATCH_SIZE = 10;
+export const TRANSACTION_GAS_LIMIT = 150000;
