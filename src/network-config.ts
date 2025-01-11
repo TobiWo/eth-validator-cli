225611967498f9332bb2cbaf5be309a51869effa
TobiWo <Emailkeeper@gmx.de>
2025-01-11 13:08:06 +0100

    feat(config): Add kurtosis network and add withdrawal contract address for mekong

--- src/network-config.ts
+++ src/network-config.ts
@@ -3,7 +3,12 @@ import { NetworkConfig } from './model/ethereum';
 export const networkConfig: Record<string, NetworkConfig> = {
   mekong: {
     consolidationContractAddress: '0x01aBEa29659e5e97C95107F20bb753cD3e09bBBb',
-    withdrawalContractAddress: '',
+    withdrawalContractAddress: '0x09Fc772D0857550724b07B850a4323f39112aAaA',
     chainId: 7078815900n
+  },
+  kurtosis: {
+    consolidationContractAddress: '0x01aBEa29659e5e97C95107F20bb753cD3e09bBBb',
+    withdrawalContractAddress: '0x09Fc772D0857550724b07B850a4323f39112aAaA',
+    chainId: 3151908n
   }
 };
