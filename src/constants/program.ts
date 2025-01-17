89fa25e04969be9488de2a93f581ad563a93a838
TobiWo <Emailkeeper@gmx.de>
2025-01-17 08:51:32 +0100

    feat(constants): Delete unnecessary batch size

--- src/constants/program.ts
+++ src/constants/program.ts
@@ -6,6 +6,5 @@ export const MIN_CONSOLIDATION_REQUEST_FEE = 1n;
 export const CONSOLIDATION_REQUEST_FEE_UPDATE_FRACTION = 17n;
 
 // Request constants
-export const BATCH_SIZE = 10;
 export const NUMBER_OF_BLOCKS_FOR_LOG_LOOKUP = 50;
 export const TRANSACTION_GAS_LIMIT = 200000;
