ce5d1c61ee893469b9c2cbe6d87d15cbd926f906
TobiWo <Emailkeeper@gmx.de>
2025-02-15 14:10:16 +0100

    refactor(constants): Move logging messages and application specific constants

---
+++ src/constants/application.ts
@@ -0,0 +1,21 @@
+// CLI Validation constants
+export const VALID_URL_PREFIXES = ['http://', 'https://'];
+
+// Request fee calculation constants
+export const MIN_CONSOLIDATION_REQUEST_FEE = 1n;
+export const CONSOLIDATION_REQUEST_FEE_UPDATE_FRACTION = 17n;
+
+// Request constants
+export const NUMBER_OF_BLOCKS_FOR_LOG_LOOKUP = 50;
+export const TRANSACTION_GAS_LIMIT = 200000n;
+export const MAX_NUMBER_OF_REQUESTS_PER_BLOCK = 220;
+
+// Withdrawal credential types
+export const WITHDRAWAL_CREDENTIALS_0x00 = '0x00';
+export const WITHDRAWAL_CREDENTIALS_0x02 = '0x02';
+
+// Beacon api endpoints
+export const VALIDATOR_STATE_BEACON_API_ENDPOINT = '/eth/v1/beacon/states/head/validators/';
+
+// General
+export const PREFIX_0x = '0x';
