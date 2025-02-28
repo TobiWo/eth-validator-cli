bdbc6c4a5d7680320d23a6b0fd4cbb3d64cc8a67
TobiWo <Emailkeeper@gmx.de>
2025-02-28 17:08:37 +0100

    feat(cli): Add withdraw subcommand

---
+++ src/cli/withdraw.ts
@@ -0,0 +1,33 @@
+import { Command } from 'commander';
+
+import { GlobalCliOptions, WithdrawOptions } from '../model/commander';
+import { withdraw } from '../service/domain/withdraw';
+import {
+  parseAndValidateValidatorPubKeys,
+  parseAndValidateWithdrawAmount
+} from '../service/validation/cli';
+
+const withdrawCommand = new Command();
+
+const validatorOptionName = 'validator';
+const amountOptionName = 'amount';
+
+withdrawCommand
+  .name('withdraw')
+  .description('Partially withdraw ETH from one or many validators')
+  .requiredOption(
+    `-v, --${validatorOptionName} <validatorPubkey...>`,
+    'Space separated list of validator pubkeys for which partially withdraws will be perfomed',
+    parseAndValidateValidatorPubKeys
+  )
+  .requiredOption(
+    `-a, --${amountOptionName} <amount>`,
+    'Amount (in ETH notation e.g. 0.001) which will be withdrawn from validator (min. 1000 GWEI / 0.000001 ETH)',
+    parseAndValidateWithdrawAmount
+  )
+  .action(async (options: WithdrawOptions, command) => {
+    const globalOptions: GlobalCliOptions = command.parent.opts();
+    await withdraw(globalOptions, options.validator, options.amount);
+  });
+
+export { withdrawCommand };
