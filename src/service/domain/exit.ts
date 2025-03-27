72d357bbbdff6809e0d7182d93d405e021a8cf04
Tobias Wohland <Emailkeeper@gmx.de>
2025-03-27 11:19:04 +0100

    feat(service): Add exit logic

---
+++ src/service/domain/exit.ts
@@ -0,0 +1,26 @@
+import chalk from 'chalk';
+
+import * as logging from '../../constants/logging';
+import { GlobalCliOptions } from '../../model/commander';
+import { withdraw } from './withdraw';
+
+/**
+ * Exit one or many validators
+ *
+ * @param globalOptions - The global cli options
+ * @param validatorPubkeys - The validator pubkey(s) which will be exited
+ */
+export async function exit(
+  globalOptions: GlobalCliOptions,
+  validatorPubkeys: string[]
+): Promise<void> {
+  logExitWarning();
+  await withdraw(globalOptions, validatorPubkeys, 0);
+}
+
+/**
+ * Log exit specific warning
+ */
+function logExitWarning(): void {
+  console.log(chalk.yellow(logging.EXIT_WARNING));
+}
